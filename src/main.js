7a5d5d088bc5d547b0ac40c07016845492d7e576
Vlad-95 <zvezdinvladislav@yandex.ru>
2019-11-22 21:29:34 +0300

    travis

--- src/main.js
+++ src/main.js
@@ -371,18 +371,18 @@ const render = (container, template, place) => {
 };
 
 
-const tripInfo = document.querySelector(".trip-info"); // обращаемся к блоку Основная информация в шапке
-render(tripInfo, createTripInfoTemplate(), "afterbegin"); // рэндерим блок основной информации
+const tripInfo = document.querySelector(`.trip-info`); // обращаемся к блоку Основная информация в шапке
+render(tripInfo, createTripInfoTemplate(), `afterbegin`); // рэндерим блок основной информации
 
-const tripControls = document.querySelector(".trip-controls"); // обращаемся к блоку c Меню и фильтром
-render(tripControls.querySelector("h2"), createMenuTemplate(), "afterend"); // рендерим меню
-render(tripControls, createFilterTemplate(), "beforeend"); // рендерим фильтры
+const tripControls = document.querySelector(`.trip-controls`); // обращаемся к блоку c Меню и фильтром
+render(tripControls.querySelector(`h2`), createMenuTemplate(), `afterend`); // рендерим меню
+render(tripControls, createFilterTemplate(), `beforeend`); // рендерим фильтры
 
-const tripEvents = document.querySelector(".trip-events"); // обращаемся к блоку основного контента страницы
-render(tripEvents.querySelector("h2"), createSortTemplate(), "afterend"); // рэндерим сортировку
-
+const tripEvents = document.querySelector(`.trip-events`); // обращаемся к блоку основного контента страницы
+render(tripEvents.querySelector(`h2`), createSortTemplate(), `afterend`); // рэндерим сортировку
+render(tripEvents, createEditItemTemplate(), `beforeend`);
 // рэндерим 3 элемента
 for (let i = 0; i < ITEM_COUNT; i++) {
-  render(tripEvents, createItemTemplate(), "beforeend");
+  render(tripEvents, createItemTemplate(), `beforeend`);
 }
 
